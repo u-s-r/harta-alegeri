174db68810fd5d70be181deefd32593473e7526a
Alex Ciobica <alex.ciobica@gmail.com>
2016-12-23 01:18:43 +0200

    Update default zoom.

--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ import voronoi from './app/voronoi';
 L.mapbox.accessToken = process.env['MAPBOX_ACCESS_TOKEN'];
 
 var map = L.mapbox.map('map', 'zetter.i73ka9hn')
-    .setView([46.88647742351024, 23.461303710937504], 10);
+    .setView([45.9432, 24.9668], 8);
 
 voronoi(map, process.env['CSV_PATH']);
